c29ad8aab3a56879921df1a26e42b8ae7463a606
Maria Mihai <maria.ic.mihai@gmail.com>
2020-03-31 10:52:14 +0200

    Lecture 46. HTTP GET with Spring RestTemplate - added the api host - added GET client method to the GET Brewery endpoint - created test to check connection

---
+++ src/test/java/guru/springframework/sbmbreweryclient/web/client/BreweryClientTest.java
@@ -0,0 +1,24 @@
+package guru.springframework.sbmbreweryclient.web.client;
+
+import guru.springframework.sbmbreweryclient.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient client;
+
+    @Test
+    void getBeerById() {
+        BeerDto beerDto = client.getBeerById(UUID.randomUUID());
+
+        assertNotNull(beerDto);
+    }
+}
